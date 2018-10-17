eaf47e77b23ee9bb9591e682e99e5405459e4897
Xkeeper <xkeeper@gmail.com>
2018-10-16 23:35:31 -0700

    [#2] Add start of revision fetching

---
+++ src/NPR/Data/Revision.php
@@ -0,0 +1,29 @@
+<?php
+
+	namespace X\NPR\Data;
+	use X\NPR\Database;
+	use X\Log;
+
+	class Revision {
+
+		protected $story		= null;
+		protected $text			= null;
+		protected $revisionId	= null;
+
+
+		public function __construct($story) {
+
+		}
+
+
+
+		public function fetch() {
+
+		}
+
+
+		protected static function getText($storyId) {
+
+		}
+
+	}
