c62cfedfa0a706f5cd819a75767b9d84c3bf26b7
Xkeeper <xkeeper@gmail.com>
2018-10-16 23:42:41 -0700

    [#2] More revision saving work

--- src/NPR/Data/Revision.php
+++ src/NPR/Data/Revision.php
@@ -6,24 +6,53 @@
 
 	class Revision {
 
-		protected $story		= null;
-		protected $text			= null;
-		protected $revisionId	= null;
+		public $revisionId	= null;
+		public $story		= null;
+		public $text		= null;
 
 
-		public function __construct($story) {
-
+		public function __construct(Story $story) {
+			$this->story	= $story;
 		}
 
 
+		public function fetchAndUpdate() {
+			$database	= Database::getDatabase();
+
+			$this->fetch();
+
+			$query		= $database->prepare("
+								INSERT INTO `revisions`
+								(
+									`revisionId`,
+									`storyId`,
+									`fetched`,
+									`text`
+								) VALUES (
+									NULL,
+									:storyId,
+									datetime('now'),
+									:text
+								)
+							");
+			$query->execute([
+					'storyId'	=> $this->story->storyId,
+					'text'		=> $this->text,
+				]);
+
+			$this->revisionId	= $database->lastInsertId();
+
+		}
 
 		public function fetch() {
-
+			$this->text	= static::getText($this->story->storyId);
+			return $this->text;
 		}
 
 
 		protected static function getText($storyId) {
-
+			$text	= file_get_contents("https://text.npr.org/s.php?sId=$storyId");
+			return $text;
 		}
 
 	}
